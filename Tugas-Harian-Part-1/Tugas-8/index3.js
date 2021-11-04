2bd8fb0407d3264cf1fd2b06917f3218b4e8dfe7
Wildanzr <graita.sukma@gmail.com>
2021-11-04 11:49:05 +0700

    update tugas 8

--- Tugas-Harian-Part-1/Tugas-8/index3.js
+++ Tugas-Harian-Part-1/Tugas-8/index3.js
@@ -1,13 +1,23 @@
 var filterBooksPromise = require('./promise2.js')
 
 const execute = async () => {
-    let soal = [
-        {berwarna: true, halaman:40},
-        {berwarna: false, halaman:250},
-        {berwarna: true, halaman:30}
-    ]
-    for(let kondisi of soal) {
-        filterBooksPromise(kondisi.berwarna, kondisi.halaman).then(function (hasil) {
+    var soalPertama = [true, 40]
+    var soalKedua = [false, 250]
+    var soalKetiga = [true, 30]
+
+    let kondisi1 = filterBooksPromise(...soalPertama).then(function (hasil) {
+        console.log(hasil)
+    }).catch(function (err) {
+        console.log(err.message)
+    })
+
+    let kondisi2 = await filterBooksPromise(...soalKedua).then(function (hasil) {
+        console.log(hasil)
+    }).catch(function (err) {
+        console.log(err.message)
+    })
+    let kondisi3 = async () => {
+        let result = await filterBooksPromise(...soalKetiga).then(function (hasil) {
             console.log(hasil)
         }).catch(function (err) {
             console.log(err.message)
